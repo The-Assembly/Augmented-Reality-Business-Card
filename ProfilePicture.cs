e67013e8b920031e05e4be60af36ef70f05df38d
Asif Rasheed <49068949+asifrasheed6@users.noreply.github.com>
2020-01-03 23:04:27 +0400

    Add files via upload

---
+++ ProfilePicture.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+/*Use the following website for Facebook ID:
+ * https://findmyfbid.in
+ */
+
+public class ProfilePicture : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(GetTexture());
+    }
+
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("http://graph.facebook.com/4/picture?type=large");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            GetComponent<Renderer>().material.mainTexture = myTexture;
+        }
+    }
+}
